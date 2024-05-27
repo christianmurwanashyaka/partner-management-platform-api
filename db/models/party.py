6580b27b088764b7b43af36f6bd25acd4e287369
Irakoze Yves <irakozeyves9@gmail.com>
2024-05-27 22:48:39 +0200

    fix: generate correct mou document

--- db/models/party.py
+++ db/models/party.py
@@ -17,3 +17,4 @@ class Party(CommonBaseModel, table=True):
     signatory: str = Field(..., description='Signatory of the party')
     mou_detail_id: Optional[uuid.UUID] = Field(default=None, foreign_key='mou_detail.uuid')
     mou_detail: Optional['MouDetail'] = Relationship(back_populates='parties', sa_relationship_kwargs={'lazy': 'selectin'})
+    position: str = Field(default='CEO', description='Position of the signatory')
--- utils/files.py
+++ utils/files.py
@@ -1,9 +1,14 @@
 import os
+import tempfile
 from datetime import datetime
 from io import BytesIO
 from itertools import chain
 
 from docx import Document
+from docx.enum.style import WD_STYLE_TYPE
+from docx.oxml.ns import qn
+from docx.oxml import OxmlElement
+from docx.shared import Pt
 from fastapi import UploadFile, HTTPException
 from openpyxl import Workbook
 from openpyxl.styles import Font
@@ -30,11 +35,121 @@ async def handle_upload_file(file: UploadFile):
         raise HTTPException(status_code=500, detail=f"Failed to save file: {str(e)}")
 
 
+# async def generate_mou_doc(mou_application, template_path):
+#     doc = Document(template_path)
+#
+#     organization = mou_application.mou_detail.project.organization
+#     project = mou_application.mou_detail.project
+#
+#     # Collect unique domains from the project's activities
+#     domains = set()
+#     for activity in project.activities:
+#         for activity_domain in activity.domains:
+#             if activity_domain.domain_intervention:
+#                 domains.add(activity_domain.domain_intervention.name)
+#     domains_str = ', '.join(domains)
+#
+#     # Collect responsibilities and signatory details for the party with organization_id
+#     responsibilities_str = ""
+#     party_signatory = ""
+#     party_position = "CEO"  # Default value
+#     for party in mou_application.mou_detail.parties:
+#         if party.organization_id:
+#             responsibilities_str = "\n".join(
+#                 [f"{idx + 1}. {responsibility}" for idx, responsibility in enumerate(party.responsibilities)])
+#             party_signatory = party.signatory
+#             print('PARTY SIGNATORY', party_signatory)
+#             party_position = party.position
+#             print('PARTY POSITION', party_position)
+#
+#     mappings = {
+#         '{ORGANIZATION_NAME}': organization.name,
+#         '{PROJECT_NAME}': project.name,
+#         '{ORGANIZATION_PO_BOX}': organization.rwanda_po_box,
+#         '{ORGANIZATION_PHONE}': organization.phone_number,
+#         '{ORGANIZATION_EMAIL}': organization.email,
+#         '{ORGANIZATION_WEBSITE}': organization.website,
+#         '{ORGANIZATION_ADDRESS}': organization.rwanda_avenue,
+#         '{OVERALL_GOAL}': project.goals[0].name if project.goals else '',
+#         '{ACTIVITIES_DOMAINS}': domains_str,
+#         '{PARTY_RESPONSIBILITIES}': responsibilities_str,
+#         '{PARTY_SIGNATORY_NAME}': party_signatory,
+#         '{PARTY_SIGNATORY_POSITION}': party_position
+#     }
+#
+#     def replace_text(paragraph, mappings):
+#         for key, value in mappings.items():
+#             if key in paragraph.text:
+#                 paragraph.text = paragraph.text.replace(key, str(value))
+#
+#     # Ensure the 'List Number' style exists
+#     if 'List Number' not in [s.name for s in doc.styles]:
+#         style = doc.styles.add_style('List Number', WD_STYLE_TYPE.PARAGRAPH)
+#         style.base_style = doc.styles['Normal']
+#         style.paragraph_format.left_indent = Pt(36)  # Set the left indentation
+#         style.paragraph_format.space_before = Pt(0)
+#         style.paragraph_format.space_after = Pt(0)
+#         style.font.name = 'Times New Roman'
+#         style.font.size = doc.styles['Normal'].font.size
+#
+#         # Set numbering for the style
+#         p = style.element
+#         num = OxmlElement('w:numPr')
+#         ilvl = OxmlElement('w:ilvl')
+#         ilvl.set(qn('w:val'), "0")
+#         numId = OxmlElement('w:numId')
+#         numId.set(qn('w:val'), "1")
+#         num.append(ilvl)
+#         num.append(numId)
+#         p.append(num)
+#
+#     def set_font(paragraph, font_name='Times New Roman'):
+#         for run in paragraph.runs:
+#             run.font.name = font_name
+#             run._element.rPr.rFonts.set(qn('w:eastAsia'), font_name)
+#
+#     # Iterate through paragraphs to replace placeholders with actual values
+#     for paragraph in doc.paragraphs:
+#         replace_text(paragraph, mappings)
+#
+#         if '{PROJECT_GOALS}' in paragraph.text:
+#             paragraph.text = paragraph.text.replace('{PROJECT_GOALS}', '')
+#             for goal in project.goals:
+#                 goal_paragraph = paragraph.insert_paragraph_before(goal.name)
+#                 goal_paragraph.style = doc.styles['List Number']
+#                 set_font(goal_paragraph)
+#
+#     # Save the modified document to a BytesIO object
+#     buffer = BytesIO()
+#     doc.save(buffer)
+#     buffer.seek(0)
+#     return buffer
+
+
 async def generate_mou_doc(mou_application, template_path):
     doc = Document(template_path)
 
     organization = mou_application.mou_detail.project.organization
     project = mou_application.mou_detail.project
+
+    # Collect unique domains from the project's activities
+    domains = set()
+    for activity in project.activities:
+        for activity_domain in activity.domains:
+            if activity_domain.domain_intervention:
+                domains.add(activity_domain.domain_intervention.name)
+    domains_str = ', '.join(domains)
+
+    # Collect responsibilities and signatory details for the party with organization_id
+    responsibilities_str = ""
+    party_signatory = ""
+    party_position = "CEO"  # Default value
+    for party in mou_application.mou_detail.parties:
+        if party.organization_id:
+            responsibilities_str = "\n".join(
+                [f"{idx + 1}. {responsibility}" for idx, responsibility in enumerate(party.responsibilities)])
+            party_signatory = party.signatory
+            party_position = party.position
 
     mappings = {
         '{ORGANIZATION_NAME}': organization.name,
@@ -43,15 +158,68 @@ async def generate_mou_doc(mou_application, template_path):
         '{ORGANIZATION_PHONE}': organization.phone_number,
         '{ORGANIZATION_EMAIL}': organization.email,
         '{ORGANIZATION_WEBSITE}': organization.website,
-        '{ORGANIZATION_ADDRESS}': organization.rwanda_avenue
+        '{ORGANIZATION_ADDRESS}': organization.rwanda_avenue,
+        '{OVERALL_GOAL}': project.goals[0].name if project.goals else '',
+        '{ACTIVITIES_DOMAINS}': domains_str,
+        '{PARTY_RESPONSIBILITIES}': responsibilities_str,
+        '{PARTY_SIGNATORY_NAME}': party_signatory,
+        '{PARTY_SIGNATORY_POSITION}': party_position
     }
 
-    for paragraph in doc.paragraphs:
-        print(f"Before: {paragraph.text}")
+    def replace_text(element, mappings):
         for key, value in mappings.items():
-            if value:
-                paragraph.text = paragraph.text.replace(key, value)
-        print(f"After: {paragraph.text}")
+            if key in element.text:
+                element.text = element.text.replace(key, str(value))
+
+    # Iterate through all elements to replace placeholders
+    for paragraph in doc.paragraphs:
+        replace_text(paragraph, mappings)
+
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for paragraph in cell.paragraphs:
+                    replace_text(paragraph, mappings)
+
+    for section in doc.sections:
+        header = section.header
+        footer = section.footer
+        for paragraph in header.paragraphs:
+            replace_text(paragraph, mappings)
+        for paragraph in footer.paragraphs:
+            replace_text(paragraph, mappings)
+
+    if 'List Number' not in [s.name for s in doc.styles]:
+        style = doc.styles.add_style('List Number', WD_STYLE_TYPE.PARAGRAPH)
+        style.base_style = doc.styles['Normal']
+        style.paragraph_format.left_indent = Pt(36)  # Set the left indentation
+        style.paragraph_format.space_before = Pt(0)
+        style.paragraph_format.space_after = Pt(0)
+        style.font.name = 'Times New Roman'
+        style.font.size = doc.styles['Normal'].font.size
+
+        p = style.element
+        num = OxmlElement('w:numPr')
+        ilvl = OxmlElement('w:ilvl')
+        ilvl.set(qn('w:val'), "0")
+        numId = OxmlElement('w:numId')
+        numId.set(qn('w:val'), "1")
+        num.append(ilvl)
+        num.append(numId)
+        p.append(num)
+
+    def set_font(paragraph, font_name='Times New Roman'):
+        for run in paragraph.runs:
+            run.font.name = font_name
+            run._element.rPr.rFonts.set(qn('w:eastAsia'), font_name)
+
+    for paragraph in doc.paragraphs:
+        if '{PROJECT_GOALS}' in paragraph.text:
+            paragraph.text = paragraph.text.replace('{PROJECT_GOALS}', '')
+            for goal in project.goals:
+                goal_paragraph = paragraph.insert_paragraph_before(goal.name)
+                goal_paragraph.style = doc.styles['List Number']
+                set_font(goal_paragraph)
 
     buffer = BytesIO()
     doc.save(buffer)
