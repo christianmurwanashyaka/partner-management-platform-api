8a38d63cccbca18046d9d5315c03508797c6698d
MURWANASHYKA Christian <chrishyaka@yahoo.fr>
2024-06-11 11:34:37 +0200

    changed main.py

--- main.py
+++ main.py
@@ -94,4 +94,4 @@ app.include_router(files.router, prefix='/api/v1/files', tags=['Files'])
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=7000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=7001, reload=True)
