fec69de2dfd925b7ea5f4509959e159233c1b90c
Irakoze Yves <irakozeyves9@gmail.com>
2024-07-25 15:35:06 +0200

    fix: blind test

--- main.py
+++ main.py
@@ -61,7 +61,8 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-origins = ["http://localhost:5173", "http://173.249.42.7:4173", "https://ihris.hisprwanda.org:4173"]
+# origins = ["http://localhost:5173", "http://173.249.42.7:4173", "https://ihris.hisprwanda.org:4173"]
+origins = ["http://localhost:5173"]
 
 app.add_middleware(
     CORSMiddleware,
