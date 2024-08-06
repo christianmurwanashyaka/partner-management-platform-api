87896b618129ae3ffbed24de672e69abd4b8a320
Irakoze Yves <irakozeyves9@gmail.com>
2024-08-06 10:27:06 +0200

    feature: mark activities as ready for report feature: get data reporter activities

--- db/models/activity.py
+++ db/models/activity.py
@@ -1,8 +1,14 @@
 import datetime
-from typing import List
+from enum import Enum
+from typing import List, Optional
 import uuid
 from sqlmodel import Field, Relationship
 from db.models.base import CommonBaseModel
+
+
+class ActivityStatus(str, Enum):
+    READY_FOR_REPORT = "ready_for_report"
+    REPORTED = "reported"
 
 
 class Activity(CommonBaseModel, table=True):
@@ -11,14 +17,15 @@ class Activity(CommonBaseModel, table=True):
     project_id: uuid.UUID = Field(default=uuid.UUID, foreign_key='project.uuid', index=True)
     project: 'Project' = Relationship(back_populates='activities', sa_relationship_kwargs={'lazy': 'selectin'})
 
-    start_date: datetime.date = Field(..., description="Start date of the project")
-    end_date: datetime.date = Field(..., description="End date of the project")
+    start_date: datetime.date = Field(..., description="Start date of the project", index=True)
+    end_date: datetime.date = Field(..., description="End date of the project", index=True)
     name: str = Field(..., description='Name of the activity', index=True)
     description: str | None = Field(default=None, nullable=True, description='Optional description of the activity')
-    implementer: str = Field(..., description='Name of the implementer of the activity')
-    implementer_unit: str = Field(..., description='Unit or group within the implementer organization')
-    fiscal_year: str = Field(..., description='Fiscal year of the activity')
+    implementer: str = Field(..., description='Name of the implementer of the activity', index=True)
+    implementer_unit: str = Field(..., description='Unit or group within the implementer organization', index=True)
+    fiscal_year: str = Field(..., description='Fiscal year of the activity', index=True)
 
     domains: List['ActivityDomain'] = Relationship(back_populates='activity', sa_relationship_kwargs={'lazy': 'selectin'})
     input_details: List['InputDetail'] = Relationship(back_populates='activity', sa_relationship_kwargs={'lazy': 'selectin'})
     operational_zones: List['OperationalZone'] = Relationship(back_populates='activity', sa_relationship_kwargs={'lazy': 'selectin'})
+    status: Optional[ActivityStatus] = Field(default=None, nullable=True, index=True)
