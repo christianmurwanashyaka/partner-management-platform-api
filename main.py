a8bf711aa0f9d744ed64dcec7b2c2422166ed3df
Irakoze Yves <irakozeyves9@gmail.com>
2024-10-30 23:26:40 +0200

    add descriptive title

--- main.py
+++ main.py
@@ -9,13 +9,42 @@ import time
 import uvicorn
 from starlette.concurrency import iterate_in_threadpool
 
-from api.endpoints import auth, budget_type, organization_type, funding_source, funding_unit, domain_intervention, \
-    input_category, sub_domain, input, organization, user, project, activity, party, mou_detail, mou_application, mou, \
-    files, sub_domain_function, sub_function, domain_data_entry, exchange_rates, statistics, report, report_activity, \
-    health
+from api.endpoints import (
+    auth,
+    budget_type,
+    organization_type,
+    funding_source,
+    funding_unit,
+    domain_intervention,
+    input_category,
+    sub_domain,
+    input,
+    organization,
+    user,
+    project,
+    activity,
+    party,
+    mou_detail,
+    mou_application,
+    mou,
+    files,
+    sub_domain_function,
+    sub_function,
+    domain_data_entry,
+    exchange_rates,
+    statistics,
+    report,
+    report_activity,
+    health,
+)
 
 from core.config import settings
-from core.logger import system_logger, api_logger, setup_exception_logging, setup_uvicorn_logging
+from core.logger import (
+    system_logger,
+    api_logger,
+    setup_exception_logging,
+    setup_uvicorn_logging,
+)
 from db.database import create_db_and_tables, async_session
 from utils.security import create_admin
 
@@ -65,9 +94,19 @@ async def lifespan(app: FastAPI):
         # Perform any cleanup tasks here
         pass
     except Exception as e:
-        system_logger.error(f"Error during shutdown: {str(e)}\n{traceback.format_exc()}")
+        system_logger.error(
+            f"Error during shutdown: {str(e)}\n{traceback.format_exc()}"
+        )
 
-app = FastAPI(lifespan=lifespan)
+
+# app = FastAPI(lifespan=lifespan)
+
+app = FastAPI(
+    lifespan=lifespan,
+    title="Partner Management Platform",
+    description="Partner MOU Management",
+    version="1.0.0",
+)
 
 app.add_middleware(
     CORSMiddleware,
@@ -101,7 +140,9 @@ async def log_requests(request: Request, call_next):
     except Exception as e:
         exception_info = f"Exception: {str(e)}\n{traceback.format_exc()}"
         # Create a JSON response for the exception
-        response = JSONResponse(status_code=500, content={"detail": "Internal server error"})
+        response = JSONResponse(
+            status_code=500, content={"detail": "Internal server error"}
+        )
 
     process_time = time.time() - start_time
 
@@ -124,33 +165,70 @@ async def log_requests(request: Request, call_next):
 
     return response
 
+
 # Include all the routers
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Authentication"])
-app.include_router(budget_type.router, prefix='/api/v1/budget_type', tags=["Budget Type"])
-app.include_router(exchange_rates.router, prefix='/api/v1/currency_exchange_rate', tags=["Currency Exchange Rate"])
-app.include_router(organization_type.router, prefix='/api/v1/organization_type', tags=["Organization Type"])
-app.include_router(funding_source.router, prefix='/api/v1/funding_source', tags=["Funding Source"])
-app.include_router(funding_unit.router, prefix='/api/v1/funding_unit', tags=["Funding Unit"])
-app.include_router(domain_intervention.router, prefix='/api/v1/domain_intervention', tags=["Domain Intervention"])
-app.include_router(sub_domain.router, prefix='/api/v1/sub_domain', tags=["Sub Domain"])
-app.include_router(sub_domain_function.router, prefix='/api/v1/sub_domain_function', tags=["Sub Domain Function"])
-app.include_router(sub_function.router, prefix='/api/v1/sub_function', tags=['Sub Function'])
-app.include_router(input_category.router, prefix='/api/v1/input_category', tags=["Input Category"])
-app.include_router(input.router, prefix='/api/v1/input', tags=["Input"])
-app.include_router(organization.router, prefix='/api/v1/organization', tags=["Organization"])
-app.include_router(user.router, prefix='/api/v1/user', tags=["User"])
-app.include_router(project.router, prefix='/api/v1/project', tags=["Project"])
-app.include_router(activity.router, prefix='/api/v1/activity', tags=["Activity"])
-app.include_router(party.router, prefix='/api/v1/party', tags=["Party"])
-app.include_router(mou_detail.router, prefix='/api/v1/mou_detail', tags=['MOU Detail'])
-app.include_router(mou_application.router, prefix='/api/v1/mou_application', tags=['MOU Application'])
-app.include_router(statistics.router, prefix='/api/v1/statistics', tags=['Statistics'])
-app.include_router(mou.router, prefix='/api/v1/mou', tags=['MOU'])
-app.include_router(files.router, prefix='/api/v1/files', tags=['Files'])
-app.include_router(domain_data_entry.router, prefix='/api/v1/domain_data_entry', tags=["Domain data entry"])
-app.include_router(report.router, prefix='/api/v1/report', tags=["Report"])
-app.include_router(report_activity.router, prefix='/api/v1/report_activity', tags=['Report Activity'])
-app.include_router(health.router, prefix='/api/v1/health', tags=['Health'])
+app.include_router(
+    budget_type.router, prefix="/api/v1/budget_type", tags=["Budget Type"]
+)
+app.include_router(
+    exchange_rates.router,
+    prefix="/api/v1/currency_exchange_rate",
+    tags=["Currency Exchange Rate"],
+)
+app.include_router(
+    organization_type.router,
+    prefix="/api/v1/organization_type",
+    tags=["Organization Type"],
+)
+app.include_router(
+    funding_source.router, prefix="/api/v1/funding_source", tags=["Funding Source"]
+)
+app.include_router(
+    funding_unit.router, prefix="/api/v1/funding_unit", tags=["Funding Unit"]
+)
+app.include_router(
+    domain_intervention.router,
+    prefix="/api/v1/domain_intervention",
+    tags=["Domain Intervention"],
+)
+app.include_router(sub_domain.router, prefix="/api/v1/sub_domain", tags=["Sub Domain"])
+app.include_router(
+    sub_domain_function.router,
+    prefix="/api/v1/sub_domain_function",
+    tags=["Sub Domain Function"],
+)
+app.include_router(
+    sub_function.router, prefix="/api/v1/sub_function", tags=["Sub Function"]
+)
+app.include_router(
+    input_category.router, prefix="/api/v1/input_category", tags=["Input Category"]
+)
+app.include_router(input.router, prefix="/api/v1/input", tags=["Input"])
+app.include_router(
+    organization.router, prefix="/api/v1/organization", tags=["Organization"]
+)
+app.include_router(user.router, prefix="/api/v1/user", tags=["User"])
+app.include_router(project.router, prefix="/api/v1/project", tags=["Project"])
+app.include_router(activity.router, prefix="/api/v1/activity", tags=["Activity"])
+app.include_router(party.router, prefix="/api/v1/party", tags=["Party"])
+app.include_router(mou_detail.router, prefix="/api/v1/mou_detail", tags=["MOU Detail"])
+app.include_router(
+    mou_application.router, prefix="/api/v1/mou_application", tags=["MOU Application"]
+)
+app.include_router(statistics.router, prefix="/api/v1/statistics", tags=["Statistics"])
+app.include_router(mou.router, prefix="/api/v1/mou", tags=["MOU"])
+app.include_router(files.router, prefix="/api/v1/files", tags=["Files"])
+app.include_router(
+    domain_data_entry.router,
+    prefix="/api/v1/domain_data_entry",
+    tags=["Domain data entry"],
+)
+app.include_router(report.router, prefix="/api/v1/report", tags=["Report"])
+app.include_router(
+    report_activity.router, prefix="/api/v1/report_activity", tags=["Report Activity"]
+)
+app.include_router(health.router, prefix="/api/v1/health", tags=["Health"])
 
 
 if __name__ == "__main__":
@@ -164,6 +242,8 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         system_logger.info("Application stopped by user (KeyboardInterrupt)")
     except Exception as e:
-        system_logger.critical(f"Application crashed: {str(e)}\n{traceback.format_exc()}")
+        system_logger.critical(
+            f"Application crashed: {str(e)}\n{traceback.format_exc()}"
+        )
     finally:
         system_logger.info("Application shutdown complete")
