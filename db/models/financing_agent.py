47b6b259809b1aa7671cb612f3ff787811e6f043
Irakoze Yves <irakozeyves9@gmail.com>
2024-11-19 23:15:02 +0200

    feedback: create the models

---
+++ db/models/financing_agent.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+import uuid
+from typing import List
+
+from sqlmodel import Field, Relationship
+
+from db.models import CommonBaseModel, Organization
+
+
+class SubFinancingAgent(CommonBaseModel, table=True):
+    __tablename__ = "sub_financing_agent"
+
+    financing_agent_uuid: uuid.UUID = Field(
+        default=uuid.UUID, foreign_key="financing_agent.uuid", index=True
+    )
+    financing_agent: "FinancingAgent" = Relationship(
+        back_populates="sub_financing_agents", sa_relationship_kwargs={"lazy": "noload"}
+    )
+    name: str = Field(..., description="Name of the sub financing agent", index=True)
+    description: str | None = Field(
+        default=None,
+        nullable=True,
+        description="Optional description of the sub financing agent",
+    )
+    sha_code: str = Field(
+        ...,
+        description="SHA code for the sub financing agent (e.g., FA.1.1)",
+        index=True,
+    )
+    organizations: List[Organization] = Relationship(
+        back_populates="sub_financing_agents", sa_relationship_kwargs={"lazy": "noload"}
+    )
+
+
+class FinancingAgent(CommonBaseModel, table=True):
+    __tablename__ = "financing_agent"
+
+    name: str = Field(..., description="Name of the financing agent", index=True)
+    description: str | None = Field(
+        default=None,
+        nullable=True,
+        description="Optional description of the financing agent",
+    )
+    sha_code: str = Field(
+        ..., description="SHA code for the financing agent (e.g., FA.1)", index=True
+    )
+    sub_financing_agents: List[SubFinancingAgent] = Relationship(
+        back_populates="financing_agent", sa_relationship_kwargs={"lazy": "noload"}
+    )
+    organizations: List[Organization] = Relationship(
+        back_populates="financing_agents", sa_relationship_kwargs={"lazy": "noload"}
+    )
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Central government",
+                "sha_code": "FA.1",
+                "description": "Central government financing agent",
+            }
+        }
---
+++ db/models/financing_scheme.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+import uuid
+from typing import List
+
+from sqlmodel import Field, Relationship
+
+from db.models import CommonBaseModel, Organization
+
+
+class SubFinancingScheme(CommonBaseModel, table=True):
+    __tablename__ = "sub_financing_scheme"
+
+    financing_scheme_uuid: uuid.UUID = Field(
+        default=uuid.UUID, foreign_key="financing_scheme.uuid"
+    )
+    financing_scheme: "FinancingScheme" = Relationship(
+        back_populates="sub_financing_schemes",
+        sa_relationship_kwargs={"lazy": "noload"},
+    )
+    name: str = Field(..., description="Name of the sub financing scheme", index=True)
+    description: str | None = Field(
+        default=None, description="Optional description for the sub financing scheme"
+    )
+    sha_code: str = Field(
+        ...,
+        description="SHA code for the sub financing scheme (e.g., FS.1.1",
+        index=True,
+    )
+    organizations: List[Organization] = Relationship(
+        back_populates="sub_financing_schemes",
+        sa_relationship_kwargs={"lazy": "noload"},
+    )
+
+
+class FinancingScheme(CommonBaseModel, table=True):
+    __tablename__ = "financing_scheme"
+
+    name: str = Field(..., description="Name of the financing scheme", index=True)
+    description: str | None = Field(
+        default=None, description="Optional description for the financing scheme"
+    )
+    sha_code: str = Field(
+        ..., description="SHA code for the financing scheme (e.g., FS.1", index=True
+    )
+    sub_financing_schemes: List[SubFinancingScheme] = Relationship(
+        back_populates="financing_scheme", sa_relationship_kwargs={"lazy": "noload"}
+    )
+    organizations: List[Organization] = Relationship(
+        back_populates="financing_schemes", sa_relationship_kwargs={"lazy": "noload"}
+    )
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Central government",
+                "sha_code": "FA.1.1",
+                "description": "Central government financing agent",
+            }
+        }
