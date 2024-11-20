f59942cc0ca965b40710919831379d97a81b29d1
Irakoze Yves <irakozeyves9@gmail.com>
2024-11-20 23:24:15 +0200

    feature: create financing stuff endpoints

--- api/endpoints/domain_intervention.py
+++ api/endpoints/domain_intervention.py
@@ -2,30 +2,49 @@ import uuid
 from datetime import datetime
 
 from fastapi import APIRouter, Request, Depends, HTTPException, status
-from sqlalchemy.orm import selectinload, joinedload, aliased, contains_eager
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.future import select
 
-from api.dependencies.access_control import admin_access, partner_access, moh_staff_access
-from api.dependencies.auth import get_current_user
+from api.dependencies.access_control import admin_access
 from db.database import get_db
-from db.models import User
 from db.models.domain import DomainIntervention, SubDomain
 from db.models.pagination import PaginatedResponse
-from schemas.domain_intervention import DomainInterventionRead, DomainInterventionList, DomainInterventionCreate
-from helpers.db import check_if_exists, get_all_items, get_first_item, get_joined_details_by_uuid
+from schemas.domain_intervention import (
+    DomainInterventionRead,
+    DomainInterventionList,
+    DomainInterventionCreate,
+)
+from helpers.db import (
+    check_if_exists,
+    get_all_items,
+    get_first_item,
+    get_joined_details_by_uuid,
+)
 
 router = APIRouter()
 
 
-@router.post('/', response_model=DomainInterventionRead, dependencies=[Depends(admin_access)])
-async def create_domain_intervention(request: Request, domain_intervention: DomainInterventionCreate, db: AsyncSession = Depends(get_db)):
+@router.post(
+    "/", response_model=DomainInterventionRead, dependencies=[Depends(admin_access)]
+)
+async def create_domain_intervention(
+    request: Request,
+    domain_intervention: DomainInterventionCreate,
+    db: AsyncSession = Depends(get_db),
+):
     user = request.state.user.email
 
     if await check_if_exists(DomainIntervention, db, name=domain_intervention.name):
-        raise HTTPException(status.HTTP_409_CONFLICT, detail='Domain intervention with this name already exists')
+        raise HTTPException(
+            status.HTTP_409_CONFLICT,
+            detail="Domain intervention with this name already exists",
+        )
 
-    new_domain_intervention = DomainIntervention(name=domain_intervention.name, description=domain_intervention.description, created_by=user)
+    new_domain_intervention = DomainIntervention(
+        name=domain_intervention.name,
+        description=domain_intervention.description,
+        created_by=user,
+    )
 
     db.add(new_domain_intervention)
     try:
@@ -36,12 +55,14 @@ async def create_domain_intervention(request: Request, domain_intervention: Doma
     return new_domain_intervention
 
 
-@router.get('/', response_model=PaginatedResponse[DomainInterventionList])
-async def get_domain_interventions(page: int = 1, page_size: int = 100, db: AsyncSession = Depends(get_db)):
+@router.get("/", response_model=PaginatedResponse[DomainInterventionList])
+async def get_domain_interventions(
+    page: int = 1, page_size: int = 100, db: AsyncSession = Depends(get_db)
+):
     return await get_all_items(db, DomainIntervention, page=page, page_size=page_size)
 
 
-@router.get('/{uuid}', response_model=DomainInterventionRead)
+@router.get("/{uuid}", response_model=DomainInterventionRead)
 async def get_domain_intervention(uuid: str, db: AsyncSession = Depends(get_db)):
     domain_intervention = await get_joined_details_by_uuid(
         db=db,
@@ -49,23 +70,33 @@ async def get_domain_intervention(uuid: str, db: AsyncSession = Depends(get_db))
         alias_model=SubDomain,
         join_condition=lambda alias: alias.domain_id == DomainIntervention.uuid,
         uuid=uuid,
-        relationship_option=DomainIntervention.subdomains
+        relationship_option=DomainIntervention.subdomains,
     )
 
     if not domain_intervention:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail='Domain intervention not found')
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND, detail="Domain intervention not found"
+        )
 
     return domain_intervention
 
 
-@router.delete('/{uuid}', response_model=DomainInterventionRead, dependencies=[Depends(admin_access)])
-async def delete_domain_intervention(uuid: uuid.UUID, request: Request, db: AsyncSession = Depends(get_db)):
+@router.delete(
+    "/{uuid}",
+    response_model=DomainInterventionRead,
+    dependencies=[Depends(admin_access)],
+)
+async def delete_domain_intervention(
+    uuid: uuid.UUID, request: Request, db: AsyncSession = Depends(get_db)
+):
     user = request.state.user.email
 
     query = select(DomainIntervention).filter(DomainIntervention.uuid == uuid)
     domain_intervention = await get_first_item(db, query)
     if not domain_intervention:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail='Domain intervention not found')
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND, detail="Domain intervention not found"
+        )
 
     domain_intervention.deleted_status = True
     domain_intervention.deleted_by = user
---
+++ api/endpoints/sub_financing_agent.py
@@ -0,0 +1,140 @@
+from datetime import datetime
+
+from fastapi import APIRouter, Depends, Request, HTTPException, status
+from sqlalchemy import select
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from api.dependencies.access_control import admin_access
+from api.dependencies.auth import get_current_user
+from db.database import get_db
+from db.models import (
+    PaginatedResponse,
+    UserRole,
+    User,
+    FinancingAgent,
+    SubFinancingAgent,
+)
+from helpers.db import check_if_exists, get_first_item, get_all_items
+from schemas.sub_financing_scheme import (
+    SubFinancingSchemeList,
+    SubFinancingSchemeCreate,
+)
+
+router = APIRouter()
+
+
+@router.post(
+    "/", response_model=SubFinancingSchemeList, dependencies=[Depends(admin_access)]
+)
+async def create_sub_financing_agent(
+    request: Request,
+    sub_financing_agent_data: SubFinancingSchemeCreate,
+    db: AsyncSession = Depends(get_db),
+):
+    try:
+        financing_agent_query = select(FinancingAgent).filter(
+            FinancingAgent.uuid == sub_financing_agent_data.financing_scheme_uuid
+        )
+        financing_agent = await get_first_item(db, financing_agent_query)
+
+        if not financing_agent:
+            raise HTTPException(
+                status.HTTP_404_NOT_FOUND, detail="Financing agent not found"
+            )
+        existing_sub_financing_agent = await check_if_exists(
+            SubFinancingAgent,
+            db,
+            name=sub_financing_agent_data.name,
+            financing_agent_uuid=sub_financing_agent_data.financing_scheme_uuid,
+        )
+        if existing_sub_financing_agent:
+            raise HTTPException(
+                status.HTTP_409_CONFLICT,
+                detail="Sub financing scheme with this name already exists",
+            )
+
+        user = request.state.user.email
+        new_sub_financing_agent = SubFinancingAgent(
+            name=sub_financing_agent_data.name,
+            description=sub_financing_agent_data.description,
+            financing_agent_uuid=sub_financing_agent_data.financing_scheme_uuid,
+            created_by=user,
+            sha_code=sub_financing_agent_data.sha_code,
+        )
+        db.add(new_sub_financing_agent)
+        await db.commit()
+        await db.refresh(new_sub_financing_agent)
+        return new_sub_financing_agent
+    except Exception as e:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+
+@router.get("/", response_model=PaginatedResponse[SubFinancingSchemeList])
+async def get_sub_financing_agents(
+    page: int = 1,
+    page_size: int = 100,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+    try:
+        if current_user.role != UserRole.DATA_MANAGER:
+            raise HTTPException(
+                status_code=403, detail="Access denied. User must be a data manager."
+            )
+        return await get_all_items(
+            db, SubFinancingAgent, page=page, page_size=page_size
+        )
+    except Exception as e:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+
+@router.get("/{uuid}", response_model=SubFinancingSchemeList)
+async def get_sub_financing_agent(
+    uuid: str,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+    try:
+        if current_user.role != UserRole.DATA_MANAGER:
+            raise HTTPException(
+                status_code=403, detail="Access denied. User must be a data manager."
+            )
+        query = select(SubFinancingAgent).filter(SubFinancingAgent.uuid == uuid)
+        sub_financing_agent = await get_first_item(db, query)
+        if not sub_financing_agent:
+            raise HTTPException(
+                status.HTTP_404_NOT_FOUND, detail="Sub financing agent not found"
+            )
+        return sub_financing_agent
+    except Exception as e:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+
+@router.delete(
+    "/{uuid}",
+    response_model=SubFinancingSchemeList,
+    dependencies=[Depends(admin_access)],
+)
+async def delete_sub_financing_scheme(
+    uuid: str, request: Request, db: AsyncSession = Depends(get_db)
+):
+    try:
+        user = request.state.user.email
+
+        query = select(SubFinancingAgent).filter(SubFinancingAgent.uuid == uuid)
+        sub_financing_agent = await get_first_item(db, query)
+        if not sub_financing_agent:
+            raise HTTPException(
+                status.HTTP_404_NOT_FOUND, detail="Sub financing scheme not found"
+            )
+
+        sub_financing_agent.deleted_status = True
+        sub_financing_agent.deleted_by = user
+        sub_financing_agent.last_updated_at = datetime.now()
+        sub_financing_agent.last_updated_by = user
+
+        await db.commit()
+        await db.refresh(sub_financing_agent)
+        return sub_financing_agent
+    except Exception as e:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
